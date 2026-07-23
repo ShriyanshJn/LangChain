f6718f19f6a0d461410faa262da09a608ca27928
Shriyansh <shriyanshjain4444@gmail.com>
2026-07-24 02:38:45 +0530

    use message

--- Prompts/chatbot.py
+++ Prompts/chatbot.py
@@ -1,19 +1,22 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model = ChatOpenAI(model = 'gpt-4o-mini')
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are a helpful AI assistant."),
+]
 
 while True:
     user_input = input("You: ")
     if user_input.lower() == 'exit':
         break
-    chat_history.append({"role": "user", "content": user_input})
+    chat_history.append(HumanMessage(content=user_input))
     response = model.invoke(chat_history)
-    chat_history.append({"role": "assistant", "content": response.content})
+    chat_history.append(AIMessage(content=response.content))
     print(f"Assistant: {response.content}")
 
 print(chat_history)
