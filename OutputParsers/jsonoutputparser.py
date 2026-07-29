6d9e2723edde94d4c223904fdd819ea97d449973
Shriyansh <shriyanshjain4444@gmail.com>
2026-07-29 22:59:43 +0530

    json output parser

---
+++ OutputParsers/jsonoutputparser.py
@@ -0,0 +1,29 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = JsonOutputParser()
+
+prompt = PromptTemplate(
+    template='Give me 5 facts about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables= {'format_instruction':parser.get_format_instructions()}
+)
+
+chain = prompt | model | parser
+
+result = chain.invoke({
+    'topic' : 'Rap Music'
+})
+
+print(result)
