0bb0e6656c5150ca8b7585d8346db99a7ab1926b
Shriyansh <shriyanshjain4444@gmail.com>
2026-08-14 02:15:51 +0530

    add length based text splitter

---
+++ TextSplitters/length_based_textsplitter.py
@@ -0,0 +1,16 @@
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader('DocumentLoaders/wild_animals.pdf')
+
+docs = loader.load()
+
+splitter = CharacterTextSplitter(
+    chunk_size=200,
+    chunk_overlap=0,
+    separator=''
+)
+
+result = splitter.split_documents(docs)
+
+print(result[7].page_content)
